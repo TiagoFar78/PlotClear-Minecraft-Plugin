70af967c742ead1c2661d32c45d1d711cee1eba4
Tiago Farinha <tiagoagostinhofarinha@gmail.com>
2023-08-23 17:23:04 +0100

    Commands added

--- src/redehexen/plotClear/commands/GiveItemSubcommand.java
+++ src/redehexen/plotClear/commands/GiveItemSubcommand.java
@@ -1,12 +1,36 @@
 package redehexen.plotClear.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import redehexen.plotClear.PlotClear;
+import redehexen.plotClear.PlotClearItem;
 
 public class GiveItemSubcommand implements PlotClearSubcommand {
 
 	@Override
 	public void execute(CommandSender sender, String cmdlabel, String[] args) {
+		if (!sender.hasPermission(PlotClear.GIVE_PERMISSION)) {
+			// TODO send message
+			return;
+		}
 		
+		if (args.length != 1) {
+			// TODO send message
+			return;
+		}
+		
+		String playerName = args[0];
+		Player player = Bukkit.getPlayer(playerName);
+		
+		if (!player.isOnline()) {
+			// TODO send message
+			return;
+		}
+		
+		PlotClearItem.giveToPlayer(player);
+		// TODO send message
 	}
 
 }
--- src/redehexen/plotClear/commands/PlotClearCommand.java
+++ src/redehexen/plotClear/commands/PlotClearCommand.java
@@ -5,11 +5,42 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class PlotClearCommand implements CommandExecutor {
+	
+	private static final String GIVE_COMMAND = "give";
+	private static final String RELOAD_COMMAND = "reload";
 
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (args.length == 0) {
+			sendGeneralUsageMessage(sender);
+			return false;
+		}
+		
+		String subCommand = args[0];
+		
+		String[] newArgs = new String[args.length - 1];
+		for (int i = 1; i < args.length; i++) {
+			newArgs[i - 1] = args[i];
+		}
+		
+		switch (subCommand.toLowerCase()) {
+		case GIVE_COMMAND:
+			new GiveItemSubcommand().execute(sender, subCommand, newArgs);
+			return false;
+		case RELOAD_COMMAND:
+			new ReloadSubcommand().execute(sender, subCommand, newArgs);
+			return false;
+		default:
+			sendGeneralUsageMessage(sender);
+			return false;
+		}
+	}
+	
+	private void sendGeneralUsageMessage(CommandSender sender) {
+		ConfigManager configManager = ConfigManager.getInstance();
+		
+		sender.sendMessage(configManager.getSetPositionUsageMessage());
+		sender.sendMessage(configManager.getReloadUsageMessage());
 	}
 
 }
