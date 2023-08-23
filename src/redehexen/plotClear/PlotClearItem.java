c82f9f7a0328be2bb3fa9cfbd4a992ccb2b0dad7
Tiago Farinha <tiagoagostinhofarinha@gmail.com>
2023-08-23 01:11:48 +0100

    PlotClear item implemented

--- src/redehexen/plotClear/PlotClearItem.java
+++ src/redehexen/plotClear/PlotClearItem.java
@@ -2,32 +2,89 @@ package redehexen.plotClear;
 
 import java.util.HashMap;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-public class PlotClearItem {
+import com.intellectualcrafters.plot.api.PlotAPI;
+import com.intellectualcrafters.plot.object.Plot;
+
+import redehexen.plotClear.managers.PlotsManager;
+
+public class PlotClearItem implements Listener {
 	
 	public static void giveToPlayer(Player player) {
-		HashMap<Integer, ItemStack> unplacedItens = player.getInventory().addItem(getItem());
+		HashMap<Integer, ItemStack> unplacedItens = player.getInventory().addItem(create());
 		
 		boolean wasPlaced = unplacedItens.size() == 0;
 		if (!wasPlaced) {
 			// TODO send message
+			return;
 		}
 	}
 	
-	private static ItemStack getItem() {
-		// TODO cria item
+	private static ItemStack create() {
+		String itemName = ;
+		
+		ItemStack item = new ItemStack(Material.GOLD_SPADE);
+		ItemMeta itemMeta = item.getItemMeta();
+		itemMeta.setDisplayName(itemName);
+		item.setItemMeta(itemMeta);
+		
+		return item;
 	}
 	
-	public static boolean isSameItem(ItemStack item2) {
-		ItemStack item1 = getItem();
+	private boolean isSameItem(ItemStack item2) {
+		ItemStack item1 = create();
 		
 		String item1Name = item1.getItemMeta().getDisplayName();
 		String item2Name = item2.getItemMeta().getDisplayName();
 		
 		return item1.getType() == item2.getType() && item1Name != null && item2Name != null &&
-				item1Name.equals(item2Name);				
+				item1Name.equals(item2Name);
+	}
+	
+	@EventHandler
+	public void playerClick(PlayerInteractEvent e) {
+		ItemStack item = e.getPlayer().getItemInHand();
+		if (!isSameItem(item)) {
+			return;
+		}
+		
+		Location loc = e.getClickedBlock().getLocation();
+		
+		PlotAPI plotAPI = PlotsManager.getInstance().getPlotsAPI();
+		Plot plot = plotAPI.getPlot(loc);
+		
+		if (plot == null || !plot.isOwner(e.getPlayer().getUniqueId())) {
+			// TODO send message
+			return;
+		}
+		
+		clearPlot(plot);
+	}
+	
+	@SuppressWarnings("deprecation")
+	private void clearPlot(Plot plot) {
+		int lowerY;
+		int higherY;
+
+		World world = Bukkit.getWorld(plot.getBottom().getWorld());
+		
+		for (int x = plot.getBottom().getX(); x <= plot.getTop().getX(); x++) {
+            for (int y = lowerY; y <= higherY; y++) {
+                for (int z = plot.getBottom().getZ(); z <= plot.getTop().getZ(); z++) {
+                    world.getBlockAt(x, y, z).setType(Material.AIR);
+                }
+            }
+        }
 	}
 
 }
